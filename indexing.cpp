85279ea71edba4a776b32df4c73ffbfeed4e06f4
Goh Zheng Ying <91591202+GohZhengYing@users.noreply.github.com>
2022-09-22 00:08:34 +0800

    Update indexing.cpp

--- indexing.cpp
+++ indexing.cpp
@@ -13,11 +13,20 @@ class Node{
     Node *_nextNode = NULL;
     //list of pointers to records(this will be empty if it is a leaf node)
     Record *_record[N];
+    
+    int _size = 0;
 
     friend class BPlusTree;
 
 public:
-    Node(){}
+    Node(){
+    
+    for(int i = 0 ;i<N;i++){
+        _key[i]=NULL;
+        _pointer[i]=NULL;
+    }
+    _pointer[N+1]=NULL;
+    }
 };
 
 class BPlusTree{
@@ -29,6 +38,310 @@ public:
     _root._leafNode = true;
     }
     void insert(Record record){
+        int nodeTrackerIndex = 0;
+        Node *nodeTracker[_height];
+        nodeTracker[nodeTrackerIndex] = _root;
+        Node *newNode = new Node;
+        while(!nodeTracker[nodeTrackerIndex]->_leafNode){
+            for(int i=0;i<N;i++){
+                if(nodeTracker[nodeTrackerIndex]->_key[i]!=NULL && nodeTracker[nodeTrackerIndex]->_key[i]>record.getValue()){
+                    nodeTrackerIndex++;
+                    nodeTracker[nodeTrackerIndex] = nodeTracker[nodeTrackerIndex-1]->_pointer[i];
+                    break;
+                }
+                else if(nodeTracker[nodeTrackerIndex]->_key[i]==NULL){
+                    nodeTrackerIndex+=1;
+                    nodeTracker[nodeTrackerIndex] = nodeTracker[nodeTrackerIndex-1]->_pointer[i];
+
+                break;
+                }
+                else if(nodeTracker[nodeTrackerIndex]->_key[i]!=NULL && nodeTracker[nodeTrackerIndex]->_key[i]<=record.getValue()&& i == N-1){
+                    nodeTrackerIndex+=1;
+                    nodeTracker[nodeTrackerIndex] = nodeTracker[nodeTrackerIndex-1]->_pointer[i+1];
+                    break;
+                }
+            }
+        }
+
+        bool emptySpace = false;
+        for(int i=0;i<N;i++){
+            if(nodeTracker[nodeTrackerIndex]->_key[i] == NULL){
+                emptySpace = true;
+            }
+        }
+        //if(record.getValue() == 11)nodeTracker[nodeTrackerIndex]->printAll();
+        if(emptySpace){
+            int temp1,temp2;
+            Record *r1,*r2,t;
+            for(int i=0;i<N;i++){
+                if(i==0 && nodeTracker[nodeTrackerIndex]->_key[i] == NULL){
+                    nodeTracker[nodeTrackerIndex]->_key[i] = record.getValue();
+                    nodeTracker[nodeTrackerIndex]->_record[i] = &record;
+                    nodeTracker[nodeTrackerIndex]->_size++;
+                    //printf(" i1 ");
+                    break;
+                }
+                else if(nodeTracker[nodeTrackerIndex]->_key[i]>record.getValue()){
+                    r1 = nodeTracker[nodeTrackerIndex]->_record[i];
+                    temp1 = nodeTracker[nodeTrackerIndex]->_key[i];
+                    nodeTracker[nodeTrackerIndex]->_record[i] = &record;
+                    nodeTracker[nodeTrackerIndex]->_key[i] = record.getValue();
+                    nodeTracker[nodeTrackerIndex]->_size++;
+                    for(int j = i+1;j<N;j++){
+                        r2 = nodeTracker[nodeTrackerIndex]->_record[j];
+                        temp2 = nodeTracker[nodeTrackerIndex]->_key[j];
+                        nodeTracker[nodeTrackerIndex]->_record[j] = r1;
+                        nodeTracker[nodeTrackerIndex]->_key[j] = temp1;
+                        r1 = r2;
+                        temp1=temp2;
+                        //cout<<"i2 "<<endl;
+
+                    }
+
+                    break;
+                }
+                else if(nodeTracker[nodeTrackerIndex]->_key[i] == NULL){
+                    nodeTracker[nodeTrackerIndex]->_key[i] = record.getValue();
+                    nodeTracker[nodeTrackerIndex]->_record[i] = &record;
+                    nodeTracker[nodeTrackerIndex]->_size++;
+                    //cout<<"i3 "<<endl;
+
+                    break;
+                }
+            }
+
+            while(nodeTrackerIndex!=0){
+                nodeTrackerIndex--;
+                Node *cursor;
+                int minVal = 0;
+                for(int i =0;i<N;i++){
+                    if(nodeTracker[nodeTrackerIndex]->_key[i]==NULL)break;
+                    cursor = nodeTracker[nodeTrackerIndex]->_pointer[i+1];
+                    while(!cursor->_leafNode)cursor = cursor->_pointer[0];
+                    nodeTracker[nodeTrackerIndex]->_key[i] = cursor->_key[0];
+                }
+            }
+            _noOfNodes++;
+            return;
+        }
+        else{
+            int temp[N+1];
+            int floorVal = floor((N+1)/2);
+            int ceilVal = ceil((N+1)/2);
+            bool recordAdded = false;
+            int keyIndex=0;
+            for(int i=0;i<N+1;i++){
+                if(!recordAdded){
+                        if(keyIndex<N && nodeTracker[nodeTrackerIndex]->_key[keyIndex]<record.getValue()){
+                            temp[i]=nodeTracker[nodeTrackerIndex]->_key[keyIndex];
+                            keyIndex++;
+                        }
+                        else{
+                            temp[i] = record.getValue();
+                            recordAdded = true;
+                        }
+                }
+                else{
+                    temp[i]=nodeTracker[nodeTrackerIndex]->_key[keyIndex];
+                    keyIndex++;
+                }
+
+            }
+
+            newNode->_leafNode = true;
+            newNode->_nextNode = nodeTracker[nodeTrackerIndex]->getNextNode();
+            nodeTracker[nodeTrackerIndex]->_nextNode = newNode;
+            nodeTracker[nodeTrackerIndex]->_size = ceilVal;
+            newNode->_size = floorVal;
+            int tempIndex=0;
+            for(int i=0;i<N;i++){
+                if(i<ceilVal){
+                nodeTracker[nodeTrackerIndex]->_key[i] = temp[tempIndex];
+                tempIndex++;
+                }
+                else{
+                nodeTracker[nodeTrackerIndex]->_key[i] = NULL;
+                }
+            }
+            for(int i=0;i<N;i++){
+                if(i<floorVal){
+                newNode->_key[i] = temp[tempIndex];
+                tempIndex++;
+                }
+                else{
+                newNode->_key[i] = NULL;
+                }
+            }
+        }
+
+        if(nodeTrackerIndex==0 && !emptySpace){
+            Node *internalNode = new Node;
+            internalNode->_leafNode = false;
+            internalNode->_pointer[0] = _root;
+            internalNode->_pointer[1] = newNode;
+            internalNode->_key[0] = internalNode->_pointer[1]->_key[0];
+            internalNode->_size++;
+            _root->_nextNode = newNode;
+            _root = internalNode;
+            _height++;
+            _noOfNodes+=2;
+            return;
+
+        }
+
+        Node *newInternalNode = newNode;
+        bool newNodesToAdd = true;
+        bool emptyPointer = false;
+
+        while(nodeTrackerIndex !=0){
+            if(newNodesToAdd){
+            _noOfNodes++;
+            for(int i=0;i<N+1;i++){
+                if(nodeTracker[nodeTrackerIndex-1]->_pointer[i] == NULL){
+                    emptyPointer = true;
+                }
+            }
+
+            if(emptyPointer){
+                for(int i=0;i<N+1;i++){
+                    if(nodeTracker[nodeTrackerIndex-1]->_pointer[i] == nodeTracker[nodeTrackerIndex]){
+                        for(int j=N;j>i;j--){
+                        nodeTracker[nodeTrackerIndex-1]->_pointer[j] = nodeTracker[nodeTrackerIndex-1]->_pointer[j-1];
+                        }
+                        nodeTracker[nodeTrackerIndex-1]->_pointer[i+1] = newInternalNode;
+                        nodeTracker[nodeTrackerIndex-1]->_size++;
+                        break;
+
+                    }
+                }
+                nodeTrackerIndex--;
+                Node *cursor;
+                for(int i =0;i<N;i++){
+                    if(nodeTracker[nodeTrackerIndex]->_key[i]==NULL)break;
+                    cursor = nodeTracker[nodeTrackerIndex]->_pointer[i+1];
+                    while(!cursor->_leafNode)cursor = cursor->_pointer[0];
+                    nodeTracker[nodeTrackerIndex]->_key[i] = cursor->_key[0];
+                    nodeTracker[nodeTrackerIndex]->_size++;
+                }
+                _noOfNodes++;
+                newNodesToAdd = false;
+            }
+            else{
+            Node *temp[N+2];
+            int floorVal = floor((N)/2);
+            int ceilVal = ceil((N)/2)+1;
+            bool pointerAdded = false;
+            int pointerIndex=0;
+            for(int i=0;i<N+3;i++){
+                if(!pointerAdded){
+                        if(pointerIndex<N+1 && nodeTracker[nodeTrackerIndex-1]->_pointer[pointerIndex] == nodeTracker[nodeTrackerIndex]){
+                            temp[i]=nodeTracker[nodeTrackerIndex-1]->_pointer[pointerIndex];
+                            temp[i+1] = newInternalNode;
+                            i++;
+                            pointerIndex+=1;
+                            pointerAdded = true;
+                        }
+                        else{
+                            temp[i] = nodeTracker[nodeTrackerIndex-1]->_pointer[pointerIndex];
+                            pointerIndex++;
+                        }
+                }
+                else{
+                    temp[i]=nodeTracker[nodeTrackerIndex]->_pointer[pointerIndex];
+                    pointerIndex++;
+                }
+
+            }
+
+            Node *internalNode = new Node;
+            internalNode->_leafNode = false;
+            int tempIndex=0;
+            nodeTracker[nodeTrackerIndex-1]->_size = ceilVal;
+            internalNode->_size = floorVal;
+            for(int i=0;i<N+1;i++){
+                if(i<=ceilVal){
+                nodeTracker[nodeTrackerIndex-1]->_pointer[i] = temp[tempIndex];
+                tempIndex++;
+                }
+                else{
+                nodeTracker[nodeTrackerIndex-1]->_pointer[i] = NULL;
+                }
+            }
+            for(int i=0;i<N+1;i++){
+                if(i<=floorVal){
+                internalNode->_pointer[i] = temp[tempIndex];
+                tempIndex++;
+                }
+                else{
+                internalNode->_pointer[i] = NULL;
+                }
+            }
+            nodeTrackerIndex--;
+                Node *cursor;
+                for(int i =0;i<N;i++){
+                    if(nodeTracker[nodeTrackerIndex]->_pointer[i+1]==NULL){
+                        nodeTracker[nodeTrackerIndex]->_key[i] = NULL;
+                    }
+                    else{
+                    cursor = nodeTracker[nodeTrackerIndex]->_pointer[i+1];
+                    while(!cursor->_leafNode)cursor = cursor->_pointer[0];
+                    nodeTracker[nodeTrackerIndex]->_key[i] = cursor->_key[0];
+                    }
+                }
+                for(int i =0;i<N;i++){
+                    if(internalNode->_pointer[i+1]==NULL)break;
+                    cursor = internalNode->_pointer[i+1];
+                    while(!cursor->_leafNode)cursor = cursor->_pointer[0];
+                    internalNode->_key[i] = cursor->_key[0];
+                }
+            newInternalNode = internalNode;
+            }
+        }
+        else{
+            nodeTrackerIndex--;
+                Node *cursor;
+                nodeTracker[nodeTrackerIndex]->_size = 0;
+                for(int i =0;i<N;i++){
+                    if(nodeTracker[nodeTrackerIndex]->_key[i]==NULL)break;
+                    nodeTracker[nodeTrackerIndex]->_size++;
+                    cursor = nodeTracker[nodeTrackerIndex]->_pointer[i+1];
+                    while(!cursor->_leafNode)cursor = cursor->_pointer[0];
+                    nodeTracker[nodeTrackerIndex]->_key[i] = cursor->_key[0];
+                }
+        }
+        }
+
+        if(newNodesToAdd){
+                Node *newRootNode = new Node;
+                newRootNode->_leafNode = false;
+                newRootNode->_pointer[0] = _root;
+                newRootNode->_pointer[1] = newInternalNode;
+                newRootNode->_size++;
+                _root = newRootNode;
+                _height++;
+                _noOfNodes++;
+
+                Node *cursor;
+                cursor = _root->_pointer[1];
+                while(!cursor->_leafNode)cursor = cursor->_pointer[0];
+                _root->_key[0] = cursor->_key[0];
+        }
+        else{
+                Node *cursor;
+                _root->_size = 0;
+                for(int i =1;i<N+1;i++){
+                if(_root->_pointer[i]==NULL){
+                        _root->_key[i-1] = NULL;
+                    }
+                    else{
+                            _root->_size++;
+                    cursor = _root->_pointer[i];
+                    while(!cursor->_leafNode)cursor = cursor->_pointer[0];
+                    _root->_key[i-1] = cursor->_key[0];
+                    }
+                }
+        }
+
     }
     void remove(Record record){
     }
