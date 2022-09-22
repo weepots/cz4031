46e94bec044713923ec9f77dfeb52edfd0261579
weepots <92163484+weepots@users.noreply.github.com>
2022-09-23 01:21:22 +0800

    create storage.h file

--- storage.cpp
+++ storage.cpp
@@ -1,1 +1,40 @@
 #include <iostream>
+#include "storage.h"
+
+using namespace std;
+
+storage::storage(int storageSize, int blkNodeSize){
+    //initialise pointers
+    char* storagePtr = nullptr;
+    this->storagePtr = new char[storageSize];
+    this->blkPtr = nullptr;
+    //initialise attributes
+    this->storageSize = storageSize;
+    this->blkNodeSize = blkNodeSize;
+    this->usedBlkSize = 0;
+    this->usedRecordSize = 0;
+    this->availBlk = storageSize/blkNodeSize;
+    this->usedBlk = 0;
+
+};
+// destroy class and clear memory
+storage:: ~ storage(){
+    delete storagePtr;
+    storagePtr = nullptr;
+};
+// record functions
+char storage::writeRecord(int recordSize){
+    if(recordSize + )
+
+};
+
+void storage::deleteRecord(char address){
+
+};
+
+bool storage::createBlock(){
+
+};
+
+
+
--- storage.h
+++ storage.h
@@ -1,9 +1,60 @@
 #define STORAGE_H
 
 const int tconst_size = 10;
-
+typedef unsigned un_int;
 
 struct Record{
-    char tconst[tconst_size];
+    char tconst[tconst_size]; //10 byte length
+    float avgRating;
+    int numVotes;
+
+};
+
+struct blkNode{
+    char address;
+    int blkSize;
+    struct blkNode* next;
+};
+
+
+
+class storage{
+private:
+    //Pointers
+    char* storagePtr;
+    char* blkPtr;
+    //Attributes in bytes
+    int storageSize;
+    int blkNodeSize;
+    int currentblkNodeSize;
+    int usedBlkSize;
+    int usedRecordSize;
+
+    //Attributes in numbers
+    int availBlk;
+    int usedBlk;
+
+public:
+    //initialise class
+    storage(int storageSize, int blkNodeSize);
+    //destroy class and clear memory
+    ~ storage();
+
+
+    //get functions
+    int getStorageSize();
+    int getblkNodeSize();
+    int getUsedBlkSize();
+    int getUsedRecordSize();
+    int getAvailBlk();
+    int getUsedBlk();
+
+    // record functions
+    char writeRecord(int recordSize);
+    void deleteRecord(char address);
+    bool createBlock();
+    
+    
+
 
 };
