762b6425bb312cbb1b30715f0d881505d15d0e09
= <pranavmasterxii@gmail.com>
2022-09-25 17:54:40 +0800

    passed in data.tsv

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <boost/algorithm/string.hpp>
 #include "storage.h"
-
+#include <cstring>
 using namespace std;
 
 
@@ -23,8 +27,31 @@ int main(){
 
     storage storage(200000000, block_size);
     // storage storage(20,5);
-
+    vector <Address> data;
     cout << storage.getAvailBlk() <<endl;
+    ifstream fin("data.tsv");
+    string line;
+    while (getline(fin,line)){
+        vector<string> parts;
+        Record record;
+        Address dataRecordAddress;
+        string tempData;
+        split(parts,line, boost::is_any_of("\t"));
+        //passed in record values
+        strcpy(record.tconst, parts[0].c_str());
+        record.avgRating = stof(parts[1]);
+        record.numVotes = stof(parts[2]);
+        //still need to pass in address(TODO)
+
+       
+
+         cout << "First of " << parts.size() << " elements: " << record.avgRating << endl;
+
+    }
+
+    fin.close();
+
+
 
 
     cout << block_size <<endl;
